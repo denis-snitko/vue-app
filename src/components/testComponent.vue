95f51fc815897a0c8a9159e5770ea755ed704422
Snitko Denis <denissnitko@gmail.com>
2021-01-27 13:06:01 +0300

    Update

---
+++ src/components/testComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    ddd
+  </div>
+</template>
+
+<script>
+// import firebase from 'firebase/app'
+
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
